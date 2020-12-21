56dda65b5a74a5e1900e18ce245f86f21b0df236
Megan Cross <megancross@Megans-MacBook-Pro.local>
2020-12-20 22:46:41 -0500

    part 1 cont.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,11 +1,12 @@
 // from data.js
-var tableData = data;
+var UFOs = data;
 
 // YOUR CODE HERE!
+//Append data to html table 
 var tbody = d3.select("tbody")
 
-tableData.forEach((ufoSighting) => {
-    console.log(ufoSighting);
+UFOs.forEach((ufoSighting) => {
+    //console.log(ufoSighting);
     var row = tbody.append("tr");
    
     Object.entries(ufoSighting).forEach(([key, value]) => {
@@ -13,3 +14,36 @@ tableData.forEach((ufoSighting) => {
         cell.text(value);
     });
 });
+
+// Select button and form
+var button = d3.select("#filter-btn");
+var form = d3.select("form") 
+
+ // Create event handlers 
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter() {
+    d3.event.preventDefault();
+    console.log("submit!");
+
+    // Select the input element 
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+
+    //Filter data 
+    var filteredData = UFOs.filter((Sighting) => Sighting.datetime === inputValue);
+    console.log(filteredData);
+
+    // Render onto page 
+    filteredData.forEach((Sighting) => {
+        var filteredrow = tbody.append("tr");
+       
+        Object.entries(Sighting).forEach(([key, value]) => {
+            var filteredcell = filteredrow.append('td');
+            filteredcell.text(value);
+        });
+    })  
+}
+
